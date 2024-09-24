14595353a1c3f1f53ecb363b652258e5c44feea4
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-09-24 13:43:26 -0400

    Add test implementation + build scripts

---
+++ test/PHEnergyCorrelatorTest.C
@@ -0,0 +1,26 @@
+/// ============================================================================
+/*! \file    PHEnergyCorrelatorTest.C
+ *  \authors Derek Anderson
+ *  \date    09.21.2024
+ *
+ *  Macro to test compiling/running PHEnergyCorrelator library.
+ */
+/// ============================================================================
+
+#define PHENERGYCORRELATORTEST_C
+
+// analysis header
+#include "../include/PHEnergyCorrelator.h"
+
+
+
+// ============================================================================
+//! Test macro for PHEnergyCorrelator library.
+// ============================================================================
+void PHEnergyCorrelatorTest() {
+
+  /* TODO write up needed unit tests here */
+
+}
+
+// end ========================================================================
