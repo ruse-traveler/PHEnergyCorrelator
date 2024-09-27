d7ccd7295512fd9d73b52cbfc71348776e9a6bce
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-09-27 18:53:43 -0400

    Add index tags, fix issue with CINT compatibility

--- include/PHCorrelatorConstants.h
+++ include/PHCorrelatorConstants.h
@@ -11,6 +11,9 @@
 #ifndef PHCORRELATORCONSTANTS_H
 #define PHCORRELATORCONSTANTS_H
 
+// c++ utilities
+#include <string>
+
 
 
 namespace PHEnergyCorrelator {
@@ -23,9 +26,33 @@ namespace PHEnergyCorrelator {
     // ------------------------------------------------------------------------
     //! Base for log axes
     // ------------------------------------------------------------------------
-    inline double const& Base() {
-       static double base = 10.;
-       return base;
+    inline double Base() {
+      const double base = 10.;
+      return base;
+    }
+
+    // ------------------------------------------------------------------------
+    //! Tag for PtJet bins
+    // ------------------------------------------------------------------------
+    inline std::string PtTag() {
+      const std::string pttag = "pt";
+      return pttag;
+    }
+
+    // ------------------------------------------------------------------------
+    //! Tag for CFJet bins
+    // ------------------------------------------------------------------------
+    inline std::string CFTag() {
+      const std::string cftag = "cf";
+      return cftag;
+    }
+
+    // ------------------------------------------------------------------------
+    //! Tag for spin bins
+    // ------------------------------------------------------------------------
+    inline std::string SpinTag() {
+      const std::string sptag = "sp";
+      return sptag;
     }
 
   }   // end Const namespace
