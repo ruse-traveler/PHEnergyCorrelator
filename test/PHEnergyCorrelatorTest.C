85291f459d1a8ce2acfc9191a34e1c924ce75607
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-10-03 14:11:39 -0400

    Add calculator test

--- test/PHEnergyCorrelatorTest.C
+++ test/PHEnergyCorrelatorTest.C
@@ -13,6 +13,8 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+// root libraries
+#include <TMath.h>
 // analysis header
 #include "../include/PHEnergyCorrelator.h"
 
@@ -73,15 +75,57 @@ void PHEnergyCorrelatorTest() {
             << std::endl;
 
   // --------------------------------------------------------------------------
+  // Test calculator
+  // --------------------------------------------------------------------------
+  /* TODO changes values to something actually physical,
+   *   these are just random...
+   */
+  std::cout << "    Case [2]: test calculator." << std::endl;
+
+  // pi/4, 5pi/4
+  const double piDiv4  = TMath::PiOver4();
+  const double pi5Div4 = 5. * TMath::PiOver4();
+
+  // jet values
+  std::vector<PHEC::Type::Jet> jets;
+  jets.push_back( PHEC::Type::Jet(0.25, 8.0, 0.2, piDiv4, 1.)   );
+  jets.push_back( PHEC::Type::Jet(0.75, 13., -0.2, pi5Div4, 1.) );
+
+  // cst values
+  std::vector< std::vector< PHEC::Type::Cst > > csts( jets.size() );
+  csts[0].push_back( PHEC::Type::Cst(0.25, 1.0, 0.25, piDiv4 + 0.02, 1.)   );
+  csts[0].push_back( PHEC::Type::Cst(0.10, 0.3, 0.18, piDiv4 - 0.02, -1.)  );
+  csts[1].push_back( PHEC::Type::Cst(0.57, 2.1, -0.16, pi5Div4 + 0.1, 1.)  );
+  csts[1].push_back( PHEC::Type::Cst(0.20, 0.9, -0.21, pi5Div4 - 0.1, -1.) );
+  csts[1].push_back( PHEC::Type::Cst(0.09, 0.1, -0.19, pi5Div4, 1.)        );
+
+  // run calculations
+  for (std::size_t ijet = 0; ijet < jets.size(); ++ijet) {
+    for (std::size_t icst_a = 0; icst_a < csts[ijet].size(); ++icst_a) {
+      for (std::size_t icst_b = 0; icst_b < csts[ijet].size(); ++icst_b) {
+
+        // skip diagonal
+        if (icst_a == icst_b) continue;
+
+        // do calculation
+        calc.CalcEEC(
+          jets[ijet],
+          std::make_pair(csts[ijet][icst_a], csts[ijet][icst_b])
+        );
+      }
+    }
+  }
+
+  // --------------------------------------------------------------------------
   // Save histograms
   // --------------------------------------------------------------------------
-  std::cout << "    Case [2]: test saving histograms" << std::endl;
+  std::cout << "    Case [3]: test saving histograms" << std::endl;
 
   // create output file
   TFile* output = new TFile("test.root", "recreate");
 
   // save histograms to output
-  calc.GetManager().SaveHists(output);
+  calc.End(output);
 
   // --------------------------------------------------------------------------
   // Tests complete
